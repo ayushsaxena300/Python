c71bac6a1d9b2cede49de902602a75a4cf25f622
ayushsaxena300 <107646719+ayushsaxena300@users.noreply.github.com>
2022-07-14 22:45:47 +0530

    Add files via upload

---
+++ Python MySirG/Programs/2. Basics of Python/4_keywords.py
@@ -0,0 +1,9 @@
+# How to see the list of KEYWORDS in python ?
+import keyword
+print(keyword.kwlist)
+
+"""
+These are the keywords in python3 :-
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
+'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+"""
