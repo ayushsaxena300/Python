79bcb87d79dbebc86839079d218948c670bf2b10
ayushsaxena300 <107646719+ayushsaxena300@users.noreply.github.com>
2022-07-27 14:15:51 +0530

    Add files via upload

---
+++ Python MySirG/Programs/2. Basics of Python/10_bitwise_operators.py
@@ -0,0 +1,23 @@
+# &(AND) operator:-
+x=5&6
+print(x)
+
+# |(OR) operator:-
+x=5|6
+print(x)
+
+# ^(XOR) operator:-
+x=5^6
+print(x)
+
+# ~(NOT) operator:-
+x=~5
+print(x)
+
+# >>(Right Shift) operator:-
+x=25>>2
+print(x)
+
+# >>(Left Shift) operator:-
+x=12<<3
+print(x)
