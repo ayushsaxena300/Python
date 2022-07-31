98274b83c11e30e4137edeb26155eaf540e3da37
ayushsaxena300 <107646719+ayushsaxena300@users.noreply.github.com>
2022-07-31 20:30:11 +0530

    Add files via upload

---
+++ Python MySirG/Programs/2. Basics of Python/14.1_input()_function.py
@@ -0,0 +1,21 @@
+# How to input user name?
+x=input("Enter your name ")
+print(x)
+
+# How to input an integer?
+x=input("Enter a number ")
+print(x)
+y=int(x)
+print(type(x),type(y))
+
+# How to input a float value?
+x=input("Enter a floating number ")
+print(x)
+y=float(x)
+print(type(x),type(y))
+
+# How to input a complex number?
+x=input("Enter a complex number ")
+print(x)
+y=complex(x)
+print(type(x),type(y))
---
+++ Python MySirG/Programs/2. Basics of Python/14.2_add_two_numbers.py
@@ -0,0 +1,5 @@
+# Program to add two numbers
+x=input("Enter first number ")
+y=input("Enter second number ")
+s=int(x)+int(y)
+print("Sum is",s)
